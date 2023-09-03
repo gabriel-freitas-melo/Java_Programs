61678a87c98ba580db7a6c64cf22a2c1c72fa5e9
gabriel-freitas-melo <gabrielfreitas1813@gmail.com>
2023-09-03 19:08:48 -0300

    Refatoração Completa com testes

--- src/fundamentos/PrimeiroPrograma.java
+++ src/fundamentos/PrimeiroPrograma.java
@@ -10,6 +10,7 @@ public class PrimeiroPrograma {
 	public static void main(String[] args) { 
 		System.out.println("Gabriel Freitas Melo");
 		System.out.println("O recomço dos estudos de JAVA");
+		System.out.println("Refatoração Completa Com Testes");
 
 	}
 
